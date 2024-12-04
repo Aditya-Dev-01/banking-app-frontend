7040f41376e80559c547c179f5ee01ddc1aed356
Aditya Pandit <adityadevtech01@gmail.com>
2024-12-04 22:25:39 +0530

    modified env

--- src/service/apiClient.ts
+++ src/service/apiClient.ts
@@ -4,13 +4,13 @@ export const apiClient = async (method: string,url_path:string, payload?: object
   try {
     if (method == "get") {
       const res = await axios.get(
-        `${process.env.BASE_URL}${url_path}`
+        `${"http://13.61.105.22:3000/api/account"}${url_path}`
       );
       const data=await res.data;
       return data;
     } else {
         const res = await axios.post(
-            `${process.env.BASE_URL}${url_path}`,payload
+            `${"http://13.61.105.22:3000/api/account"}${url_path}`,payload
           );
           const data=await res.data;
           return data;
