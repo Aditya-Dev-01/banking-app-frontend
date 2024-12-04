782cb7d93ec6868a4dd933cbd668117d79e06047
Aditya Pandit <adityadevtech01@gmail.com>
2024-12-04 22:35:31 +0530

    modified test

--- src/service/apiClient.ts
+++ src/service/apiClient.ts
@@ -4,9 +4,12 @@ export const apiClient = async (method: string,url_path:string, payload?: object
   try {
     if (method == "get") {
       const res = await axios.get(
-        `${"http://13.61.105.22:3000/api/account"}${url_path}`
+        // `${"http://13.61.105.22:3000/api/account"}${url_path}`
+         `${"https://fakestoreapi.com/products/"}`
+       
       );
       const data=await res.data;
+      console.log('data',data);
       return data;
     } else {
         const res = await axios.post(
